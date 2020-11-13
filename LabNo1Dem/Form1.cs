d4bfc70c2101a30af1dcbbca039d61d712a0903c
opilane <opilane@HP-CZC6297B89>
2020-11-13 15:43:36 +0200

    Add some elements and add function to elements

--- LabNo1Dem/Form1.cs
+++ LabNo1Dem/Form1.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,10 +14,12 @@ namespace LabNo1Dem
 {
     public partial class Form1 : Form
     {
+        Panel panel;
         Label lbl1, lbl2, lbl3;
         ComboBox combox1, combox2;
         ListBox lbox1, lbox2, lbox3;
         Button btnl, btnr, btnAl, btnAr, btnr11, btnr21, btnr12, btnr22, btnr13, btnr23, btnf, btnc, btne, btns;
+        GroupBox gbox1, gbox2;
         TextBox txt;
         CheckBox cbox1, cbox2;
         RichTextBox rtxt;
@@ -26,182 +30,293 @@ namespace LabNo1Dem
             Width = 1000;
             Text = "Лабораторная работа №1";
 
+            panel = new Panel();
+            panel.BorderStyle = BorderStyle.Fixed3D;
+            panel.Size = new Size(540, 730);
+            panel.Location = new Point(10, 20);
+            Controls.Add(panel);
+
             lbl1 = new Label();
             lbl1.Text = "Раздел 1";
             lbl1.Size = new Size(60, 20);
-            lbl1.Location = new Point(30, 20);
-            Controls.Add(lbl1);
+            lbl1.Location = new Point(20, 20);
+            panel.Controls.Add(lbl1);
 
             lbl2 = new Label();
             lbl2.Text = "Раздел 2";
             lbl2.Size = new Size(60, 20);
             lbl2.Location = new Point(350, 20);
-            Controls.Add(lbl2);
+            panel.Controls.Add(lbl2);
 
             combox1 = new ComboBox();
             combox1.Text = "Сортировка по...";
             combox1.Size = new Size(180, 20);
-            combox1.Location = new Point(30, 50);
-            Controls.Add(combox1);
+            combox1.Location = new Point(20, 50);
+            panel.Controls.Add(combox1);
 
             combox2 = new ComboBox();
             combox2.Text = "Сортировка по...";
             combox2.Size = new Size(180, 20);
-            combox2.Location = new Point(350, 50);
-            Controls.Add(combox2);
+            combox2.Location = new Point(340, 50);
+            panel.Controls.Add(combox2);
 
             lbox1 = new ListBox();
             lbox1.Size = new Size(180, 300);
-            lbox1.Location = new Point(30, 80);
-            Controls.Add(lbox1);
+            lbox1.Location = new Point(20, 80);
+            panel.Controls.Add(lbox1);
 
             lbox2 = new ListBox();
             lbox2.Size = new Size(180, 300);
-            lbox2.Location = new Point(350, 80);
-            Controls.Add(lbox2);
+            lbox2.Location = new Point(340, 80);
+            panel.Controls.Add(lbox2);
 
             btnl = new Button();
             btnl.Text = "<";
             btnl.Size = new Size(120, 30);
-            btnl.Location = new Point(220, 140);
-            Controls.Add(btnl);
+            btnl.Location = new Point(210, 140);
+            panel.Controls.Add(btnl);
 
             btnr = new Button();
             btnr.Text = ">";
             btnr.Size = new Size(120, 30);
-            btnr.Location = new Point(220, 180);
-            Controls.Add(btnr);
+            btnr.Location = new Point(210, 180);
+            panel.Controls.Add(btnr);
 
             btnAl = new Button();
             btnAl.Text = "<<";
             btnAl.Size = new Size(120, 30);
-            btnAl.Location = new Point(220, 220);
-            Controls.Add(btnAl);
+            btnAl.Location = new Point(210, 220);
+            panel.Controls.Add(btnAl);
 
             btnAr = new Button();
             btnAr.Text = ">>";
             btnAr.Size = new Size(120, 30);
-            btnAr.Location = new Point(220, 260);
-            Controls.Add(btnAr);
+            btnAr.Location = new Point(210, 260);
+            panel.Controls.Add(btnAr);
 
             btnr11 = new Button();
             btnr11.Text = "Сортировать";
             btnr11.Size = new Size(120, 40);
-            btnr11.Location = new Point(60, 390);
-            Controls.Add(btnr11);
+            btnr11.Location = new Point(50, 390);
+            panel.Controls.Add(btnr11);
 
             btnr21 = new Button();
             btnr21.Text = "Очистить";
             btnr21.Size = new Size(120, 40);
-            btnr21.Location = new Point(60, 440);
-            Controls.Add(btnr21);
+            btnr21.Location = new Point(50, 440);
+            btnr21.Click += Btnr21_Click;
+            panel.Controls.Add(btnr21);
 
             btnr12 = new Button();
             btnr12.Text = "Добавить";
             btnr12.Size = new Size(120, 40);
-            btnr12.Location = new Point(220, 390);
-            Controls.Add(btnr12);
+            btnr12.Location = new Point(210, 390);
+            btnr12.Click += Btnr12_Click;
+            panel.Controls.Add(btnr12);
 
             btnr22 = new Button();
             btnr22.Text = "Удалить";
             btnr22.Size = new Size(120, 40);
-            btnr22.Location = new Point(220, 440);
-            Controls.Add(btnr22);
+            btnr22.Location = new Point(210, 440);
+            panel.Controls.Add(btnr22);
 
             btnr13 = new Button();
             btnr13.Text = "Сортировать";
             btnr13.Size = new Size(120, 40);
-            btnr13.Location = new Point(380, 390);
-            Controls.Add(btnr13);
+            btnr13.Location = new Point(370, 390);
+            panel.Controls.Add(btnr13);
 
             btnr23 = new Button();
             btnr23.Text = "Очистить";
             btnr23.Size = new Size(120, 40);
-            btnr23.Location = new Point(380, 440);
-            Controls.Add(btnr23);
+            btnr23.Location = new Point(370, 440);
+            btnr23.Click += Btnr23_Click;
+            panel.Controls.Add(btnr23);
+
+            gbox1 = new GroupBox();
+            gbox1.Text = "Поиск";
+            gbox1.Size = new Size(320, 220);
+            gbox1.Location = new Point(30, 500);
+            panel.Controls.Add(gbox1);
 
             lbl3 = new Label();
             lbl3.Text = "Введите искомое слово";
             lbl3.Size = new Size(150, 20);
-            lbl3.Location = new Point(40, 540);
-            Controls.Add(lbl3);
+            lbl3.Location = new Point(20, 30);
+            gbox1.Controls.Add(lbl3);
 
             txt = new TextBox();
             txt.Size = new Size(200, 20);
-            txt.Location = new Point(40, 560);
-            Controls.Add(txt);
+            txt.Location = new Point(20, 50);
+            gbox1.Controls.Add(txt);
 
             lbox3 = new ListBox();
-            lbox3.Size = new Size(200, 150);
-            lbox3.Location = new Point(40, 590);
-            Controls.Add(lbox3);
+            lbox3.Size = new Size(200, 140);
+            lbox3.Location = new Point(20, 80);
+            gbox1.Controls.Add(lbox3);
 
             cbox1 = new CheckBox();
             cbox1.Text = "Раздел 1";
             cbox1.Size = new Size(100, 20);
-            cbox1.Location = new Point(250, 610);
-            Controls.Add(cbox1);
+            cbox1.Location = new Point(230, 90);
+            gbox1.Controls.Add(cbox1);
 
             cbox2 = new CheckBox();
-            cbox2.Text = "Раздел 1";
+            cbox2.Text = "Раздел 2";
             cbox2.Size = new Size(100, 20);
-            cbox2.Location = new Point(250, 640);
-            Controls.Add(cbox2);
+            cbox2.Location = new Point(230, 120);
+            gbox1.Controls.Add(cbox2);
 
             btnf = new Button();
             btnf.Text = "Поиск";
-            btnf.Size = new Size(100, 60);
-            btnf.Location = new Point(250, 678);
-            Controls.Add(btnf);
+            btnf.Size = new Size(80, 50);
+            btnf.Location = new Point(230, 160);
+            btnf.Click += Btnf_Click;
+            gbox1.Controls.Add(btnf);
 
             btnc = new Button();
             btnc.Text = "Сброс";
             btnc.Size = new Size(160, 60);
-            btnc.Location = new Point(380, 610);
-            Controls.Add(btnc);
+            btnc.Location = new Point(370, 590);
+            btnc.Click += Btnc_Click;
+            panel.Controls.Add(btnc);
 
             btne = new Button();
             btne.Text = "Выход";
             btne.Size = new Size(160, 60);
-            btne.Location = new Point(380, 680);
-            Controls.Add(btne);
+            btne.Location = new Point(370, 660);
+            btne.Click += Btne_Click;
+            panel.Controls.Add(btne);
 
             rtxt = new RichTextBox();
             rtxt.Size = new Size(400, 600);
             rtxt.Location = new Point(560, 20);
             Controls.Add(rtxt);
 
+            gbox2 = new GroupBox();
+            gbox2.Text = "Выбор слов";
+            gbox2.Size = new Size(400, 120);
+            gbox2.Location = new Point(560, 630);
+            Controls.Add(gbox2);
+
             rbtn1 = new RadioButton();
             rbtn1.Text = "Все";
             rbtn1.Size = new Size(100, 20);
-            rbtn1.Location = new Point(590, 650);
-            Controls.Add(rbtn1);
+            rbtn1.Location = new Point(10, 20);
+            gbox2.Controls.Add(rbtn1);
 
             rbtn2 = new RadioButton();
             rbtn2.Text = "Содержащие цифры";
             rbtn2.Size = new Size(150, 20);
-            rbtn2.Location = new Point(590, 680);
-            Controls.Add(rbtn2);
+            rbtn2.Location = new Point(10, 50);
+            gbox2.Controls.Add(rbtn2);
 
             rbtn3 = new RadioButton();
             rbtn3.Text = "Содержащие e-mail";
             rbtn3.Size = new Size(150, 20);
-            rbtn3.Location = new Point(590, 710);
-            Controls.Add(rbtn3);
+            rbtn3.Location = new Point(10, 80);
+            gbox2.Controls.Add(rbtn3);
 
             btns = new Button();
             btns.Text = "Начать";
             btns.Size = new Size(100, 80);
-            btns.Location = new Point(860, 650);
-            Controls.Add(btns);
+            btns.Location = new Point(290, 25);
+            btns.Click += Btns_Click;
+            gbox2.Controls.Add(btns);
 
             MainMenu menu = new MainMenu();
             MenuItem menuitem1 = new MenuItem("Файл");
-            menuitem1.MenuItems.Add("Открыть  Ctrl+O");
-            menuitem1.MenuItems.Add("Сохранить  Ctrl+S");
+            menuitem1.MenuItems.Add("Открыть  Ctrl+O", new EventHandler(menuitem1_Open));
+            menuitem1.MenuItems.Add("Сохранить  Ctrl+S", new EventHandler(menuitem1_Save));
             menuitem1.MenuItems.Add("Выход  Alt+X", new EventHandler(menuitem1_Exit));
+            MenuItem menuitem2 = new MenuItem("?", new EventHandler(menuitem2_Info));
             menu.MenuItems.Add(menuitem1);
+            menu.MenuItems.Add(menuitem2);
             Menu = menu;
+        }
+
+        private void Btnr12_Click(object sender, EventArgs e)
+        {
+            Form2 AddRec = new Form2();
+            AddRec.Owner = this;
+            AddRec.ShowDialog();
+        }
+
+        private void Btnf_Click(object sender, EventArgs e)
+        {
+            lbox3.Items.Clear();
+            string Find = txt.Text;
+            if (cbox1.Checked)
+            {
+                foreach(string String in lbox1.Items)
+                {
+                    if (String.Contains(Find)) lbox3.Items.Add(String);
+                }
+            }
+            if (cbox2.Checked)
+            {
+                foreach(string String in lbox2.Items)
+                {
+                    if (String.Contains(Find)) lbox3.Items.Add(String);
+                }
+            }
+        }
+
+        private void Btnr23_Click(object sender, EventArgs e)
+        {
+            lbox2.Items.Clear();
+        }
+
+        private void Btnr21_Click(object sender, EventArgs e)
+        {
+            lbox1.Items.Clear();
+        }
+
+        private void Btne_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Ты точно хочешь закрыть программу?", "Выход", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+
+        private void Btnc_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Ты точно хочешь сбросить все параметры?", "Сброс", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                lbox1.Items.Clear();
+                lbox2.Items.Clear();
+                lbox3.Items.Clear();
+                rtxt.Clear();
+                rbtn1.Checked = false;
+                rbtn2.Checked = false;
+                rbtn3.Checked = false;
+                cbox1.Checked = false;
+                cbox2.Checked = false;
+            }
+        }
+
+        private void Btns_Click(object sender, EventArgs e)
+        {
+            lbox1.Items.Clear();
+            lbox2.Items.Clear();
+            lbox1.BeginUpdate();
+            string[] Strings = rtxt.Text.Split(new char[] { '\n', '\t', ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            foreach(string s in Strings)
+            {
+                string Str = s.Trim();
+                if (Str == String.Empty) continue;
+                if (rbtn1.Checked) lbox1.Items.Add(Str);
+                if (rbtn2.Checked)
+                {
+                    if (Regex.IsMatch(Str, @"\d")) lbox1.Items.Add(Str);
+                }
+                if (rbtn3.Checked)
+                {
+                    if (Regex.IsMatch(Str, @"\w+@\w+\.\w+")) lbox1.Items.Add(Str);
+                }
+            }
+            lbox1.EndUpdate();
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -214,6 +329,35 @@ namespace LabNo1Dem
             {
                 Application.Exit();
             }
+        }
+        private void menuitem2_Info(object sender, EventArgs e)
+        {
+            MessageBox.Show("Программу создавал Сандер Демиховский из группы TARpv19");
+        }
+        private void menuitem1_Open(object sender, EventArgs e)
+        {
+            OpenFileDialog OpenDlg = new OpenFileDialog();
+            if(OpenDlg.ShowDialog() == DialogResult.OK)
+            {
+                StreamReader Reader = new StreamReader(OpenDlg.FileName, Encoding.Default);
+                rtxt.Text = Reader.ReadToEnd();
+                Reader.Close();
+            }
+            OpenDlg.Dispose();
+        }
+        private void menuitem1_Save(object sender, EventArgs e)
+        {
+            SaveFileDialog SaveDlg = new SaveFileDialog();
+            if(SaveDlg.ShowDialog() == DialogResult.OK)
+            {
+                StreamWriter Writer = new StreamWriter(SaveDlg.FileName);
+                for(int i = 0; i < lbox2.Items.Count; i++)
+                {
+                    Writer.WriteLine((string)lbox2.Items[i]);
+                }
+                Writer.Close();
+            }
+            SaveDlg.Dispose();
         }
     }
 }
---
+++ LabNo1Dem/Form2.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LabNo1Dem
+{
+    public partial class Form2 : Form
+    {
+        GroupBox gbox;
+        RadioButton rbtn1, rbtn2;
+        Label lbl;
+        TextBox txt;
+        Button btna, btnc;
+        public Form2()
+        {
+            Height = 205;
+            Width = 300;
+            Text = "Добавление записи";
+
+            gbox = new GroupBox();
+            gbox.Text = "Добавить в";
+            gbox.Size = new Size(260, 70);
+            gbox.Location = new Point(10, 10);
+            Controls.Add(gbox);
+
+            rbtn1 = new RadioButton();
+            rbtn1.Text = "Раздел 1";
+            rbtn1.Size = new Size(80, 20);
+            rbtn1.Location = new Point(10, 20);
+            gbox.Controls.Add(rbtn1);
+
+            rbtn2 = new RadioButton();
+            rbtn2.Text = "Раздел 2";
+            rbtn2.Size = new Size(80, 20);
+            rbtn2.Location = new Point(10, 40);
+            gbox.Controls.Add(rbtn2);
+
+            lbl = new Label();
+            lbl.Text = "Введите текст:";
+            lbl.Size = new Size(90, 20);
+            lbl.Location = new Point(10, 85);
+            Controls.Add(lbl);
+
+            txt = new TextBox();
+            txt.Size = new Size(260, 20);
+            txt.Location = new Point(10, 105);
+            Controls.Add(txt);
+
+            btna = new Button();
+            btna.Text = "Добавить";
+            btna.Size = new Size(120, 25);
+            btna.Location = new Point(10, 130);
+            btna.Click += Btna_Click;
+            Controls.Add(btna);
+
+            btnc = new Button();
+            btnc.Text = "Отмена";
+            btnc.Size = new Size(120, 25);
+            btnc.Location = new Point(150, 130);
+            Controls.Add(btnc);
+        }
+
+        private void Btna_Click(object sender, EventArgs e)
+        {
+            //Form1 Main = this.Owner as Form1;
+            //if(txt.Text != "")
+            //{
+            //    if (this.rbtn1.Checked == true)
+            //        Main.lbox1.Items.Add(this.txt.Text);
+            //    else Main.lbox2.Items.Add(this.txt.Text);
+            //    this.Close();
+            //}
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
